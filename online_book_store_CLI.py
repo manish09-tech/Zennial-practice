4c9a4ad84510270fe9e664a7e1dbb67e552eff11
Manish Jakkula <manishsai.jakkula@thezennialpro.com>
2025-06-10 12:26:37 +0530

    Finished task coding

--- online_book_store_CLI.py
+++ online_book_store_CLI.py
@@ -1,8 +1,8 @@
-import json
-import logging
-from datetime import datetime
-from functools import wraps
+import json # json used to import data or write
+import logging # Logging is to apply logger data
+from functools import wraps # wraps is imported to handle exceptions 
 
+# Handles logger data in given format
 logging.basicConfig(
     filename='bookstore_app.log',
     level=logging.INFO,
@@ -11,6 +11,8 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+
+# Handles exceptions
 def handle_exceptions(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -21,6 +23,7 @@ def handle_exceptions(func):
             logger.exception(f"Error while Executing: {func.__name__} : {e}")
     return wrapper
 
+# Creates class for book
 class Book:
     def __init__(self, book_id, title, author, genre, price, published_date, stock):
         self.book_id = book_id
@@ -31,6 +34,7 @@ class Book:
         self.published_date = published_date
         self.stock = stock
 
+    # fetch the stock values of the  books
     def is_in_stock(self):
         return self.stock > 0
 
@@ -41,6 +45,7 @@ def load_books(filepath):
         books = json.load(f)
         return [Book(**b) for b in books]
 
+# function to print book store menu
 def showcli():
     while True:
         print("\n ===== Book Store Menu =====")
@@ -50,15 +55,18 @@ def showcli():
 
         choice = input("Please Select an Option : ")
 
+        # Condition to list all books at store
         if choice == "1":
             for book in books:
                 print(f"{book.book_id} - {book.title} by {book.author} - {book.genre} - ${book.price} - Published: {book.published_date} - Stock: {book.stock}")
 
+        # Condition to find books in stock
         elif choice == "2":
             for book in books:
                 if book.is_in_stock():
                     print(f"{book.book_id} - {book.title} - In Stock: {book.stock}")
          
+        # Condition to exit from menu
         elif choice == "3":
             print("Exiting Bookstore CLI. Thank you!")
             break
