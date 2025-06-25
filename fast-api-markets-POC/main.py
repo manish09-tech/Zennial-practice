7282897a3bf691be1ace7603ce0e44219ede5c10
Manish Jakkula <manishsai.jakkula@thezennialpro.com>
2025-06-25 14:55:13 +0530

    Markets_POC: Completed

--- fast-api-markets-POC/main.py
+++ fast-api-markets-POC/main.py
@@ -11,19 +11,19 @@ async def create_cus_account(data: CreateAccountRequest):
     result = await create_customer_account(data.customer_id)
     
     if not result:
-        raise HTTPException(status_code=400, detail="Customer already exists")
-    return {"message": f"Customer {data.customer_id} created with empty cart "}
+        raise HTTPException(status_code=400, detail="The following customer ID is already exists.")
+    return {"message": f"A customer with ID {data.customer_id} has been created."}
 
 @app.post("/add-items")
 async def add_to_cart(data: AddCartRequest):
     products = await add_cart(data.customer_id, data.product)
-    return {"message" : f"added product to cart with id {data.customer_id} with {data.product} products and available products in the cart is {products} "}
+    return {"message" : f"Customer with ID: {data.customer_id} added {data.product} items to their cart. The cart now contains {products} available items."}
 
 @app.post("/remove-items")
 async def remove_from_cart(data: RemoveCartRequest):
     try:
         products = await remove_product(data.customer_id, data.product)
-        return {"message" : f"{data.product} products are removed from id {data.customer_id} remaining products are {products}"}
+        return {"message" : f"Removed {data.product} items from customer ID {data.customer_id}. {products} items remain in the cart."}
     except ValueError as e:
         raise HTTPException (status_code=400, detail= str(e))
     except Exception as e:
