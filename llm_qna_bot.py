7539dc657fc715786bf7a8d01a939b0903e4cb2f
Manish Jakkula <manishsai.jakkula@thezennialpro.com>
2025-06-27 14:36:51 +0530

    qna_bot: In Progress

--- llm_qna_bot.py
+++ llm_qna_bot.py
@@ -1,10 +1,29 @@
 import os
 import pymupdf as fitz
-import numpy as np
 
+CHUNK_SIZE = 40
+
+def chunk_text(text):
+    words= text.split()
+    return [" ".join(words[i: i + CHUNK_SIZE]) for i in range(0, len(words), CHUNK_SIZE)]
+
+def extract_from_pdf(pdf_path):
+    pages= fitz.open(pdf_path)
+    full_text= "\n".join([page.get_text() for page in pages])
+    return full_text
 
 def add_document():
-    return True
+    pdf_path= input ("enter pdf path here: ").strip()
+    if not os.path.exists(pdf_path):
+        print(f"File not found {pdf_path}")
+        return
+    text= extract_from_pdf(pdf_path)
+
+    if not text:
+        print ("content not found!!")
+        return
+    
+    text_chunks = chunk_text (text)
 
 def query():
     return True
@@ -17,11 +36,11 @@ def main():
     while True:
         print ("\nSelect any options : ")
         print ("1. add document to faiss index")
-        print ("2.query document")
+        print ("2. query document")
         print ("3. delete document")
         print ("4. exit")
 
-        choice= input("Please select an option (1/2/3/4) :")
+        choice= input("\nPlease select an option (1/2/3/4) :")
 
         if choice=="1":
             add_document()
