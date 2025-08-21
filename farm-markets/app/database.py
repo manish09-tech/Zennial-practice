e4ebe887f467d10358ad35b266713c6cb1586cfc
Manish Jakkula <manishsai.jakkula@thezennialpro.com>
2025-08-21 12:39:27 +0530

    farm-markets: In progress

---
+++ farm-markets/app/database.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# load environment
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+if not MONGO_URL:
+    raise RuntimeError("MONGO_URL not set in environment or .env")
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["farm_markets_db"]
+
+users_collection = db.get_collection("users")
+farmers_collection = db.get_collection("farmers")
+products_collection = db.get_collection("products")
+orders_collection = db.get_collection("orders")
