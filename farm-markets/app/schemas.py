9e2bdefb2c3b149c130cdfa0f7beaeda8185afd2
Manish Jakkula <manishsai.jakkula@thezennialpro.com>
2025-08-21 13:00:42 +0530

    farm-markets: In progress

--- farm-markets/app/schemas.py
+++ farm-markets/app/schemas.py
@@ -9,3 +9,62 @@ class UserCreate(BaseModel):
     password: str
     role: str # farmer or buyer
 
+class UserOut(BaseModel):
+    _id: str = Field(alias="_id")
+    name: str
+    email: EmailStr
+    role: str
+    created_at: datetime
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+
+    class LoginData(BaseModel):
+        email: EmailStr
+        password: str
+
+    class Token(BaseModel):
+        access_token: str
+        token_type: str = "bearer"
+
+
+# Products
+class ProductCreate(BaseModel):
+    name: str
+    price: float
+    quantity: int
+
+class ProductOut(BaseModel):
+    _id: str = Field(alias="_id")
+    name: str
+    price: float
+    quantity: int
+    created_at: datetime
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+
+# Orders
+class OrderCreate(BaseModel):
+    product_id: str
+    quantity: int
+
+class OrderOut(BaseModel):
+    _id: str = Field(alias="_id")
+    product_id: str
+    buyer_id: str
+    quantity: int
+    total_price: float
+    created_at: datetime
+    
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
